1435379deedba3c5b4996caecb39fb445790c97a
M.Baumgärtner <klagges2011@googlemail.com>
2023-03-09 19:03:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,11 +29,11 @@ echo " #!/bin/sh" >> /opt/login/logout.sh
 echo "java -jar /opt/login/Login.jar $VAR_USER $VAR_PW EIN" >> /opt/login/login.sh
 echo "java -jar /opt/login/Login.jar $VAR_USER $VAR_PW AUS" >> /opt/login/logout.sh
 
-echo " 30 16	* * mon,tue,wed,thu,fri root /opt/login/logout.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
-echo " 12 08	* * mon,tue,wed,thu,fri root /opt/login/login.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
+echo "30 16	* * mon,tue,wed,thu,fri root /opt/login/logout.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
+echo "12 08	* * mon,tue,wed,thu,fri root /opt/login/login.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
 echo ""
 echo " Scripte werden ab sofort via Crontab gestartet"
-echo " siehe /etc/crontab "
+echo " siehe /etc/crontab hier können auch zeitliche änderungen eingetragen werden. "
 chmod 644 /opt/login/login.sh
 chmod 644 /opt/login/logout.sh
 
