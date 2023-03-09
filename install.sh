23d89f9d4b3598b19302a93d0a39d411e8c63483
M.Baumgärtner <klagges2011@googlemail.com>
2023-03-09 18:50:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-
-
+# Installations Script für Moodle Auto Login
+clear
+echo " Prüfe ob auch als Root angemeldet"
+echo.
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-read -p 'Benutzername für Moodle anmeldung eingeben: ' VAR_USER
-read -p 'Moodle Passwort eingeben' VAR_PW 
-
+echo "Installatios Script für Moodle Auto login"
+echo.
+echo.
+read -p 'Benutzername für Moodle anmeldung eingeben:  ' VAR_USER
+echo.
+read -p 'Moodle Passwort eingeben:  ' VAR_PW 
+echo.
+echo " System wird aktualisiert und notwendige Pakete installiert."
 
 
 apt-get update && apt-get upgrade -y 
 apt-get install  openjdk-11-jre mc -y
 mkdir /opt/login
-wget https://github.com/mbaumgae81/auto_login_moodle/raw/master/bin/Login.jar /opt/login/
-
+wget https://github.com/mbaumgae81/auto_login_moodle/raw/master/bin/Login.jar -P /opt/login/
+echo.
+echo "Erzeuge Scripts"
 echo " #!/bin/sh" >> /opt/login/login.sh
-echo " #!/bin/sh" >> /opt/login/lout.sh
+echo " #!/bin/sh" >> /opt/login/logut.sh
 
-echo "java -jar /opt/login/login.jar $VAR_USER $VAR_PW EIN" >> /opt/login/login.sh
-echo "java -jar /opt/login/login.jar $VAR_USER $VAR_PW AUS" >> /opt/login/logout.sh
+echo "java -jar /opt/login/Login.jar $VAR_USER $VAR_PW EIN" >> /opt/login/login.sh
+echo "java -jar /opt/login/Login.jar $VAR_USER $VAR_PW AUS" >> /opt/login/logout.sh
 
-echo " 30 16	* * mon,tue,wed,thu,fri root /opt/login/logout.sh >> /var/log/jaav.log  2>&1" >> /opt/login/crontab
-echo " 12 08	* * mon,tue,wed,thu,fri root /opt/login/login.sh >> /var/log/jaav.log  2>&1" >> /opt/login/crontab
+echo " 30 16	* * mon,tue,wed,thu,fri root /opt/login/logout.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
+echo " 12 08	* * mon,tue,wed,thu,fri root /opt/login/login.sh >> /var/log/jaav.log  2>&1" >> /etc/crontab
+
 chmod 644 /opt/login/login.sh
 chmod 644 /opt/login/logout.sh
 
